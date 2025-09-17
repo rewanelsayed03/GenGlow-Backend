25a23477da7a8d149d2bf424993c8bc295dea5b2
rewanelsayed03 <rewan.elsayed2003@gmail.com>
2025-09-18 00:54:23 +0300

    Prepare for rebase: add local changes

--- app.js
+++ app.js
@@ -29,7 +29,5 @@ app.use('/api/shipping-partners', require('./routes/shippingPartners'));
 
 
 
-
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
