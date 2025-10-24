ce56653105b59d33ece52fc9ca82fd79b41fbf7f
rewanelsayed03 <rewan.elsayed2003@gmail.com>
2025-10-24 15:54:58 +0300

    Updated

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(fileUpload({ useTempFiles: true }));
+app.use(fileUpload({ useTempFiles: true, tempFileDir: '/tmp/' }));
 
 // Cloudinary
 app.post('/api/upload', async (req, res) => {
